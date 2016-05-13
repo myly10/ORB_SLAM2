e8e5cec674f66d451d4936d1e93152c6ac763ab4
YangLiu <myly10@gmail.com>
2016-05-13 23:39:25 +0800

    DBoW2 fromBinary bug fix

--- Thirdparty/DBoW2/DBoW2/TemplatedVocabulary.h
+++ Thirdparty/DBoW2/DBoW2/TemplatedVocabulary.h
@@ -232,6 +232,8 @@ namespace DBoW2{
 		 */
 		bool loadFromTextFile(const std::string &filename);
 
+		bool loadFromBinaryFile(const std::string &filename);
+
 		/**
 		 * Saves the vocabulary into a text file
 		 * @param filename
@@ -1199,10 +1201,99 @@ namespace DBoW2{
 		return c;
 	}
 
+	template<class TDescriptor, class F>
+	bool TemplatedVocabulary<TDescriptor,F>::loadFromTextFile(const std::string &filename)
+	{
+		ifstream f;
+		f.open(filename.c_str());
+
+		if(f.eof())
+			return false;
+
+		m_words.clear();
+		m_nodes.clear();
+
+		string s;
+		getline(f,s);
+		stringstream ss;
+		ss << s;
+		ss >> m_k;
+		ss >> m_L;
+		int n1, n2;
+		ss >> n1;
+		ss >> n2;
+
+		if(m_k<0 || m_k>20 || m_L<1 || m_L>10 || n1<0 || n1>5 || n2<0 || n2>3)
+		{
+			std::cerr << "Vocabulary loading failure: This is not a correct text file!" << endl;
+			return false;
+		}
+
+		m_scoring = (ScoringType)n1;
+		m_weighting = (WeightingType)n2;
+		createScoringObject();
+
+		// nodes
+		int expected_nodes =
+				(int)((pow((double)m_k, (double)m_L + 1) - 1)/(m_k - 1));
+		m_nodes.reserve(expected_nodes);
+
+		m_words.reserve(pow((double)m_k, (double)m_L + 1));
+
+		m_nodes.resize(1);
+		m_nodes[0].id = 0;
+		while(!f.eof())
+		{
+			string snode;
+			getline(f,snode);
+			stringstream ssnode;
+			ssnode << snode;
+
+			int nid = m_nodes.size();
+			m_nodes.resize(m_nodes.size()+1);
+			m_nodes[nid].id = nid;
+
+			int pid ;
+			ssnode >> pid;
+			m_nodes[nid].parent = pid;
+			m_nodes[pid].children.push_back(nid);
+
+			int nIsLeaf;
+			ssnode >> nIsLeaf;
+
+			stringstream ssd;
+			for(int iD=0;iD<F::L;iD++)
+			{
+				string sElement;
+				ssnode >> sElement;
+				ssd << sElement << " ";
+			}
+			F::fromString(m_nodes[nid].descriptor, ssd.str());
+
+			ssnode >> m_nodes[nid].weight;
+
+			if(nIsLeaf>0)
+			{
+				int wid = m_words.size();
+				m_words.resize(wid+1);
+
+				m_nodes[nid].word_id = wid;
+				m_words[wid] = &m_nodes[nid];
+			}
+			else
+			{
+				m_nodes[nid].children.reserve(m_k);
+			}
+		}
+
+		return true;
+
+	}
+
 // --------------------------------------------------------------------------
 
 	template<class TDescriptor, class F>
-	bool TemplatedVocabulary<TDescriptor, F>::loadFromTextFile(const std::string &filename){
+	bool TemplatedVocabulary<TDescriptor, F>::loadFromBinaryFile(const std::string &filename){
 		ifstream f;
 		f.open(filename.c_str(), ios::binary);
 
@@ -1256,13 +1347,9 @@ namespace DBoW2{
 			bool nIsLeaf;
 			f.read(&nIsLeaf, sizeof(char));
 
-			stringstream ssd;
-			for (int iD=0; iD<F::L; iD++){
-				unsigned char sElement;
-				f.read(&sElement, sizeof(char));
-				ssd<<(int)sElement<<" ";
-			}
-			F::fromString(m_nodes[nid].descriptor, ssd.str());
+			unsigned char *sElement=new unsigned char[F::L];
+			F::fromBinary(m_nodes[nid].descriptor, sElement);
+			delete[] sElement;
 
 			f.read(&m_nodes[nid].weight, sizeof(double));
 
